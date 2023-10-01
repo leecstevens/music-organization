e514af4d91b9837078489573b616d559a49492e1
lee <39603399+leecstevens@users.noreply.github.com>
2023-10-01 12:38:25 -0500

    Cache and sort all unique data for DB

--- music_to_sql.py
+++ music_to_sql.py
@@ -18,12 +18,93 @@
 
 import shared
 import sqlite3
+import re, os
+import music_tag
+from mutagen.mp3 import MP3
+
+all_artists = []
+diff_artists = []
+all_titles = []
+diff_titles = []
+all_albums = []
+diff_albums = []
+all_paths = []
+diff_paths = []
+all_names = []
+diff_names = []
+
+class cache:
+    def prep_cache(thelist):
+        for file in thelist:
+            path = os.path.split(file)[0]
+            name = os.path.split(file)[1]
+            track = get_song_data(file)
+            if track['artist'] not in all_artists:
+                if track['artist'] not in diff_artists:
+                    diff_artists.append(track['artist'])
+            if track['album'] not in all_albums:
+                if track['album'] not in diff_albums:
+                    diff_albums.append(track['album'])
+            if track['title'] not in all_titles:
+                if track['title'] not in diff_titles:
+                    diff_titles.append(track['title'])
+            if path not in all_paths:
+                if path not in diff_paths:
+                    diff_paths.append(path)
+            if name not in all_names:
+                if path not in diff_names:
+                    diff_names.append(name)
+
+
+def convert_length(length):
+    pass
+
+def clean_song_data(lyric):
+    re_ticket = 'See (.*) LiveGet tickets as low as \$(.*)You might also like'
+    printed = False
+    trimnum = 0
+    if lyric[-1].isnumeric():
+        printed = True
+        done = False
+        trimnum = 1
+        while not done:
+            if lyric[-(trimnum+1)].isnumeric():
+                trimnum += 1
+            else:
+                done = True
+    lyric = re.sub(re_ticket, '', lyric)
+    if trimnum > 0:
+        lyric = lyric[:-trimnum]
+    lyric = lyric.replace('<br>You might also like','<br>')
+    lyric = lyric.replace('<br><br>','<br>')
+    return lyric
+
+def get_song_data(file):
+    f = music_tag.load_file(file)
+    ex = MP3(file)
+    song = {
+    'path': os.path.split(file)[0],
+    'name': os.path.split(file)[1], 
+    'artist': str(f['artist']),
+    'title': str(f["title"]),
+    'album': str(f["album"]),
+    'lyrics': clean_song_data(str(f['lyrics'])) if str(f['lyrics']) != '' else '',
+    'length': round(ex.info.length),
+    'bitrate': ex.info.bitrate
+    }
+    return song
 
 music_folder = shared.settings.get('music_folder')
 dbname = shared.settings.get('sqlitedb')
 conn = shared.sqlite.connect(dbname)
-print (music_folder)
 filelist = shared.file.list_path(music_folder,True)
-for i in range(5):
-    print('/'.join(filelist[i].split('/')[1:]))
+cache.prep_cache(filelist)
+print('Tracks: %s\nUnique Artists: %s\nUnique Albums: %s\nUnique Titles: %s\nUnique Paths: %s\nUnique Names: %s' % (
+    len(filelist),
+    len(all_artists)+len(diff_artists),
+    len(all_albums)+len(diff_albums),
+    len(all_titles)+len(diff_titles),
+    len(all_paths)+len(diff_paths),
+    len(all_names)+len(diff_names)
+))
 conn.close()
